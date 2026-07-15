54a346e687ee0d729d443f6cd82a7c4d2d0dfb2d
Abu Huraira <abuhuraira1961@gmail.com>
2026-07-15 21:43:57 +0500

    .

--- sqlite_devtool_api/extension/devtools/build/flutter_bootstrap.js
+++ sqlite_devtool_api/extension/devtools/build/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2234679164" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1587021756" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
